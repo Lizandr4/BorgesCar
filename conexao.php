fedc6f3b80ee65b4f65869a5e79610d29d18dff8
Lizandr4 <133605445+Lizandr4@users.noreply.github.com>
2023-06-15 07:49:10 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -6,13 +6,28 @@ $dbName='concessionaria';
 
 $conexao  = new mysqli($dbhost, $dbUsername, $dbPassword, $dbName);
 
-if($conexao-> connect_errno)
+/*if($conexao-> connect_errno)
 {
     echo"Erro";
 }
 else
 {
     echo"Conexão efetuada com sucesso";
-}
+}*/
+
+
+    /*if(isset($_POST['submit'])){
+
+
+    include_once(conexao.php);
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $numero = $_POST['numero'];
+    $mensagem = $_POST['mensagem'];
+
+    $result = mysqli_query($conexao, "INSERT INTO cliente(nome,email,numero,mensagem) VALUES('$nome','$email','$numero','$mensagem')");
+   
+   }*/
 
 ?>
